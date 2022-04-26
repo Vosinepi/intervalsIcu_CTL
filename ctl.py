cbfa7e39da376bd1bf96d3eb800277115f4465de
Vosinepi <85812110+Vosinepi@users.noreply.github.com>
2022-04-26 10:43:20 -0300

    fin?

--- ctl.py
+++ ctl.py
@@ -1,11 +1,12 @@
 import json
-import tkinter as tk
+from tkinter import *
+import tkinter
 from urllib import response
 import requests
 
 # datos
-api_token = "reemplazar por API_KEY"
-id_atleta = "id_atleta"
+api_token = "api_key"
+id_atleta = "ID"
 
 # request
 response = requests.get(
@@ -15,7 +16,15 @@ response = requests.get(
 
 # obtencion de CTL y ATL y redondeo a dos decimales
 data = response.json()
-print(json.dumps(data[-1], indent=4, sort_keys=True))
+print(json.dumps(data[-1]["ctl"], indent=4, sort_keys=True))
+i = -1
+semanaCtl = []
+
+while i < 0 and i >= -8:
+
+    semanaCtl.append(json.dumps(data[i]["ctl"], indent=4, sort_keys=True))
+    i -= 1
+print(semanaCtl)
 ctl_largo = float(json.dumps(data[-1]["ctl"], indent=4, sort_keys=True))
 atl_largo = float(json.dumps(data[-1]["atl"], indent=4, sort_keys=True))
 
@@ -23,23 +32,23 @@ ctl_corto = round(ctl_largo, 2)
 atl_corto = round(atl_largo, 2)
 
 # crear ventana
-window = tk.Tk()
+window = tkinter.Tk()
 
 
 # window.attributes('-fullscreen', True)
 window.attributes("-alpha", 0.5)
-window.geometry("200x210+1720+0")
+window.geometry("150x290+1770+0")
 window.resizable(False, False)
 window.update_idletasks()
 window.overrideredirect(True)
 
 # crear text label de titulo para CTL
-label = tk.Label(window, text="CTL", font=("Arial 15"))
+label = tkinter.Label(window, text="CTL", font=("Arial 15"))
 label.pack()
 
 # funcion muestra de CTL
 def text(n):
-    label = tk.Label(text=(str(n)), font="Arial 25", fg="red")
+    label = tkinter.Label(text=(str(n)), font="Arial 25", fg="red")
     label.master.overrideredirect(True)
     label.master.lift()
 
@@ -50,12 +59,12 @@ def text(n):
 text(ctl_corto)
 
 # crear text label de titulo para ATL
-label = tk.Label(window, text="ATL", font=("Arial 15"))
+label = tkinter.Label(window, text="ATL", font=("Arial 15"))
 label.pack()
 
 # funcion muestra de ATL
 def atl(n):
-    label = tk.Label(text=(str(n)), font="Arial 25", fg="red")
+    label = tkinter.Label(text=(str(n)), font="Arial 25", fg="red")
     label.master.overrideredirect(True)
     label.master.lift()
 
@@ -66,12 +75,12 @@ def atl(n):
 atl(atl_corto)
 
 # crear text label de titulo para FORMA
-label = tk.Label(window, text="FORMA", font=("Arial 15"))
+label = tkinter.Label(window, text="FORMA", font=("Arial 15"))
 label.pack()
 
 # funcion FORMA
 def forma(n):
-    label = tk.Label(text=(str(n)), font="Arial 25", fg="red")
+    label = tkinter.Label(text=(str(n)), font="Arial 25", fg="red")
     label.master.overrideredirect(True)
     label.master.lift()
 
@@ -81,5 +90,20 @@ def forma(n):
 
 forma(round((ctl_corto - atl_corto), 2))
 
+# crear text label de titulo para RAMPA
+label = tkinter.Label(window, text="RAMPA", font=("Arial 15"))
+label.pack()
+
+# funcion RAMPA
+def rampa(n):
+    label = tkinter.Label(text=(str(n)), font="Arial 25", fg="red")
+    label.master.overrideredirect(True)
+    label.master.lift()
+
+    label.master.wm_attributes("-transparentcolor", "white")
+    label.pack()
+
+
+rampa(round((float(semanaCtl[0]) - float(semanaCtl[7])), 2))
 
 window.mainloop()
